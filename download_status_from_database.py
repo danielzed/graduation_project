20704b5c1fb2bb141b8f38e8c5e79e4cde81bbc9
danielzed <18601636496@163.com>
2016-04-19 17:00:57 +0800

    get the original fenci product

---
+++ download_status_from_database.py
@@ -0,0 +1,36 @@
+#-*- coding:utf-8 -*-
+import MySQLdb
+import jieba
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
+
+#id列表，原创内容分词结果列表，昵称列表
+id_list=[]
+corpus=[]
+name_list=[]
+
+
+#链接陈明朝数据库
+conn=MySQLdb.connect(host="115.29.55.54",port=3306,user="root",passwd="chen724467110",charset="utf8",db="b5test")
+cursor=conn.cursor()
+sqlstr="select uuid,userName from user where uuid is not null;"
+n=cursor.execute(sqlstr)
+#取得数据库内用户id和昵称列表
+for row in cursor.fetchall():
+	id_list.append(row[0])
+	name_list.append(row[1].encode('utf-8'))
+	index=0
+#获得每个用户的内容和转发原因等原创内容
+for id in id_list:
+	sqlstr="select content,tran_reason from userStatus where uuid='%s';" % id
+	n=cursor.execute(sqlstr)
+	content=""
+	a=0
+	
+	for status in cursor.fetchall():
+		a+=1
+		content=content+status[0].encode('utf-8')+status[1].encode('utf-8')
+	seg_list=jieba.cut(content)
+	corpus.append(' '.join(seg_list))
+for row in corpus:
+	print row.encode('utf-8')
