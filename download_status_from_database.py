ffa080a3608279f15ae1650f9beb6ea7693e46d5
danielzed <18601636496@163.com>
2016-04-20 11:06:30 +0800

    delete the stopwords from corpus and add the dictionary

--- download_status_from_database.py
+++ download_status_from_database.py
@@ -9,7 +9,11 @@ id_list=[]
 corpus=[]
 name_list=[]
 
-
+#获取停用词列表
+stopwords=[]
+with open("dictionary/stopwords.txt") as f:
+	for row in f.readlines():
+		stopwords.append(row.strip().decode('utf-8'))
 #链接陈明朝数据库
 conn=MySQLdb.connect(host="115.29.55.54",port=3306,user="root",passwd="chen724467110",charset="utf8",db="b5test")
 cursor=conn.cursor()
@@ -25,12 +29,16 @@ for id in id_list:
 	sqlstr="select content,tran_reason from userStatus where uuid='%s';" % id
 	n=cursor.execute(sqlstr)
 	content=""
-	a=0
-	
+	word_no_stopwords=""
 	for status in cursor.fetchall():
-		a+=1
-		content=content+status[0].encode('utf-8')+status[1].encode('utf-8')
+		content=content+status[0]+status[1]
 	seg_list=jieba.cut(content)
-	corpus.append(' '.join(seg_list))
+	for word in seg_list:
+		if word not in stopwords:
+			word_no_stopwords=word_no_stopwords+" "+word
+	corpus.append(word_no_stopwords)
 for row in corpus:
 	print row.encode('utf-8')
+
+# for row in corpus:
+# 	print row.encode('utf-8')
